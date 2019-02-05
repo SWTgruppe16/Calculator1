b060a3064f997bdfc31f15b42d1ebf7e7a134066
Laura Noergaard <au559056@post.au.dk>
2019-02-05 13:29:15 +0100

    From q to a

--- Calculator1/Calculator.cs
+++ Calculator1/Calculator.cs
@@ -8,9 +8,9 @@ namespace Calculator
 {
     public class Calculator
     {
-        public double Add(double q, double b)
+        public double Add(double a, double b)
         {
-            return q + b;
+            return a + b;
         }
 
         public double Subtract(double a, double b)
