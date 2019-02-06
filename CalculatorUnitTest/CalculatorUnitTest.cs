9f6cbd5f19a6e817a707bea2688a425c5050b04a
henriettepilegaard <201507843@post.au.dk>
2019-02-06 01:11:04 +0100

    Small design changes

--- CalculatorUnitTest/CalculatorUnitTest.cs
+++ CalculatorUnitTest/CalculatorUnitTest.cs
@@ -18,10 +18,11 @@ namespace CalculatorUnitTest
            public void Setup()
             {
               uut = new Calculator.Calculator();
-
             }
     
-            //Test add With Different Values
+
+
+        //Test add With Different Values
             [TestCase(2,4,6)]
             [TestCase(-2,4,2)]
             [TestCase(-4,-4,-8)]
@@ -30,7 +31,9 @@ namespace CalculatorUnitTest
             Assert.That(uut.Add(a, b), Is.EqualTo(result));
             }
 
-            //Test overload add
+
+
+        //Test overload add
             [TestCase(1, 5)]
             [TestCase(10, 14)]
             [TestCase(-10, -6)]
@@ -42,7 +45,9 @@ namespace CalculatorUnitTest
                 Assert.That(uut.Add(a), Is.EqualTo(result));
             }
 
-            //Test Subtract With Different Values
+
+
+        //Test Subtract With Different Values
             [TestCase(2,4,2)]
             [TestCase(5,4,-1)]
             [TestCase(-2,-2,0)]
@@ -51,7 +56,8 @@ namespace CalculatorUnitTest
             Assert.That(uut.Subtract(a, b), Is.EqualTo(result));
             }
 
-            //Test subtract overload
+
+        //Test subtract overload
             [TestCase(2, -2)]
             [TestCase(-2, -6)]
             [TestCase(10, 6)]
@@ -62,7 +68,8 @@ namespace CalculatorUnitTest
             }
 
 
-            //Test Multiply with different values 
+
+        //Test Multiply with different values 
             [TestCase(5,5,25)]
             [TestCase(5,-5,-25)]
             [TestCase(-5, -5, 25)]
@@ -71,18 +78,20 @@ namespace CalculatorUnitTest
                 Assert.That(uut.Multiply(a, b), Is.EqualTo(result));
             }
 
-            //Test multiply overload
+
+
+        //Test multiply overload
             [TestCase(2, 8)]
             [TestCase(-2, -8)]
             [TestCase(10, 40)]
-
             public void Test_Multiply_WithDifferentValuesAndAccumulator4(double a, double result)
             {
                 uut.Add(2 + 2);
                 Assert.That(uut.Multiply(a), Is.EqualTo(result));
             }
 
-            //Test power With different values
+
+        //Test power With different values
             [TestCase(2,2,4)]
             [TestCase(2,5,32)]
             [TestCase(2,-3,0.125)]
@@ -91,11 +100,10 @@ namespace CalculatorUnitTest
             Assert.That(uut.Power(x, exp), Is.EqualTo(result));
             }
 
-            //Test power overload
+        //Test power overload
             [TestCase(2, 16)]
             [TestCase(0, 0)]
             [TestCase(-2, 16)]
-
             public void Test_Power_WithDifferntValuesAndAccumulator4(double a, double result)
             {
                 uut.Add(2 + 2);
@@ -103,7 +111,8 @@ namespace CalculatorUnitTest
             }
             // hvis der skal ingå en acceptabel varriabel kan kan skrive(resultat).within(0.01));
 
-            //Test divide with different values 
+
+        //Test divide with different values 
             [TestCase(4,2,2)]
             [TestCase(8,2,4)]
             [TestCase(-4,2,-2)]
@@ -113,7 +122,8 @@ namespace CalculatorUnitTest
                 Assert.That(uut.Divide(dividend, divisor), Is.EqualTo(result));
             }
 
-            //Test DivideByZeroException
+
+        //Test DivideByZeroException
             [TestCase(10, 0)]
             [TestCase(5, 0)]
             [TestCase(0, 0)]
@@ -122,14 +132,14 @@ namespace CalculatorUnitTest
                 Assert.Throws<DivideByZeroException>(() => uut.Divide(dividend, divisor));
             }
         
-            //Test fresh Accumulator
+        //Test fresh Accumulator
             [TestCase()]
             public void Test_Fresh_Accumulator()
             {
                 Assert.That(uut.Accumulator, Is.EqualTo(0));
             }
 
-            //Test accumulator after adding 
+        //Test accumulator after adding 
             [TestCase(2, 2, 4)]
             [TestCase(3, 3, 6)]
             public void Test_Accumulator_After_Add(double a, double b, double c)
@@ -138,7 +148,7 @@ namespace CalculatorUnitTest
                 Assert.That(uut.Accumulator, Is.EqualTo(c));
             }
 
-            //Test Clear
+        //Test Clear
             [TestCase()]
             public void Test_Clear_ResultIsZero()
             {
